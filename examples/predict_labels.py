1bd85ca38621c01cc625619c836cbd286258ae64
Damon Cai <damoncai.319@gmail.com>
2019-01-10 18:21:35 +1100

    Changed to work with AllenNLP

--- examples/predict_labels.py
+++ examples/predict_labels.py
@@ -1,15 +1,34 @@
 import logging
 import os
-from typing import List
+from typing import List, Sequence, Dict
+
+from torch.nn.functional import binary_cross_entropy_with_logits, cross_entropy
 
 from examples.run_classifier import NLPCC2018Processor, convert_examples_to_features, InputExample
-from pytorch_pretrained_bert import BertTokenizer, BertForSequenceClassification
+from pytorch_pretrained_bert import BertTokenizer, BertForSequenceClassification, BertModel, BertConfig
 import torch
 from torch.utils.data import TensorDataset, DataLoader, SequentialSampler
 import numpy as np
 from torch import nn
-from skorch import NeuralNetClassifier
 import pandas as pd
+
+import allennlp.models
+from allennlp.data import Instance
+from allennlp.data.fields import TextField, SequenceLabelField
+from allennlp.data.dataset_readers import DatasetReader
+from allennlp.common.file_utils import cached_path
+from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
+from allennlp.data.tokenizers import Token
+from allennlp.data.vocabulary import Vocabulary
+from allennlp.modules.text_field_embedders import TextFieldEmbedder, BasicTextFieldEmbedder
+from allennlp.modules.token_embedders import Embedding
+from allennlp.modules.seq2seq_encoders import Seq2SeqEncoder, PytorchSeq2SeqWrapper
+from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
+from allennlp.training.metrics import CategoricalAccuracy
+from allennlp.data.iterators import BucketIterator
+from allennlp.training.trainer import Trainer
+from allennlp.predictors import SentenceTaggerPredictor
+
 
 DATA_DIR = "../data/nlpcc2018"
 EVAL_BATCH_SIZE = 8
@@ -45,105 +64,47 @@ logger.info("device: {} n_gpu: {}, distributed training: {}, 16-bits training: {
     device, n_gpu, bool(LOCAL_RANK != -1), FP16))
 
 
+class BertForAllenNLP(BertModel, allennlp.models.Model):
+    def __init__(self, config: BertConfig, num_labels: int = 2, multi_label: bool = False):
+        super(BertForAllenNLP, self).__init__(config)
+        self.num_labels = num_labels
+        self.multi_label = multi_label
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, num_labels)
+        self.apply(self.init_bert_weights)
+
+    def forward(self, input_ids: torch.Tensor, token_type_ids: torch.Tensor = None,
+                attention_mask: torch.Tensor = None, labels: torch.Tensor = None,
+                class_weight: torch.Tensor = None) -> Dict[str, torch.Tensor]:
+        _, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        output_dict = {"logits": logits}
+
+        if labels is not None:
+            if self.multi_label:
+                loss_fct = binary_cross_entropy_with_logits(pos_weight=class_weight)
+                loss = loss_fct(logits.view(-1, self.num_labels), labels)
+            else:
+                loss_fct = cross_entropy(weight=class_weight)
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            output_dict["loss"] = loss
+        else:
+            return output_dict
+
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        # TODO: Implement multi-label metrics.
+        #   (either ranking-based or label-based: if label-based, need to do logits -> predictions)
+        return {}
+
+
 # Load a trained model that you have fine-tuned
 output_model_file = os.path.join(OUTPUT_DIR, "pytorch_model.bin")
 model_state_dict = torch.load(output_model_file, map_location='cpu')  # Modify if running on GPU
-model = BertForSequenceClassification.from_pretrained(BERT_MODEL, state_dict=model_state_dict, num_labels=len(label_list), multi_label=True)
+model = BertForAllenNLP.from_pretrained(BERT_MODEL, state_dict=model_state_dict,
+                                        num_labels=len(label_list), multi_label=True)
 model.to(device)
-
-
-def eval_and_predict(examples: List[InputExample], multi_label, batch_size=8, eval=True):
-    features = convert_examples_to_features(examples, label_list, MAX_SEQ_LENGTH, tokenizer,
-                                                 multi_label=multi_label)
-    logger.info("***** Running evaluation *****")
-    logger.info("  Num examples = %d", len(examples))
-    logger.info("  Batch size = %d", batch_size)
-    all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
-    all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
-    all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
-    all_label_ids = torch.tensor([f.label_id for f in features],
-                                 dtype=torch.float if multi_label else torch.long)
-    eval_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
-    # Run prediction for full data
-    eval_sampler = SequentialSampler(eval_data)
-    eval_dataloader = DataLoader(eval_data, sampler=eval_sampler, batch_size=batch_size)
-
-    model.eval()
-    eval_loss = 0
-    nb_eval_steps, nb_eval_examples = 0, 0
-    all_logits = []
-    for input_ids, input_mask, segment_ids, label_ids in eval_dataloader:
-        input_ids = input_ids.to(device)
-        input_mask = input_mask.to(device)
-        segment_ids = segment_ids.to(device)
-        label_ids = label_ids.to(device)
-
-        with torch.no_grad():
-            tmp_eval_loss = model(input_ids, segment_ids, input_mask, label_ids)
-            logits = model(input_ids, segment_ids, input_mask)
-
-        logits = logits.detach().cpu().numpy()
-        all_logits.append(logits)
-        label_ids = label_ids.to('cpu').numpy()
-
-        eval_loss += tmp_eval_loss.mean().item()
-
-        nb_eval_examples += input_ids.size(0)
-        nb_eval_steps += 1
-
-    eval_loss = eval_loss / nb_eval_steps
-
-    result = {'eval_loss': eval_loss,}
-
-    logger.info("***** Eval results *****")
-    for key in sorted(result.keys()):
-        logger.info("  %s = %s", key, str(result[key]))
-
-    # Combine logits and convert to probabilities
-    all_logits = np.concatenate(all_logits, axis=0)
-    if multi_label:
-        all_probs = nn.Sigmoid()(torch.tensor(all_logits))
-    else:
-        # In single-label case probabilities must sum to 1, therefore softmax is used
-        all_probs = nn.Softmax()(torch.tensor(all_logits))
-    all_probs = all_probs.numpy()
-
-    # # Write probabilities to file
-    # print("Writing probabilities to {}:".format(output_probs_file))
-    # all_probs_as_df = pd.DataFrame(data=all_probs, columns=label_list)
-    # all_probs_as_df.to_csv(output_probs_file, sep='\t')
-
-    return all_probs
-
-
-def eval_and_predict_skorch(examples: List[InputExample], multi_label, batch_size=8, eval=True):
-    features = convert_examples_to_features(examples, label_list, MAX_SEQ_LENGTH, tokenizer,
-                                                 multi_label=multi_label)
-    logger.info("***** Running evaluation *****")
-    logger.info("  Num examples = %d", len(examples))
-    logger.info("  Batch size = %d", batch_size)
-    all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
-    all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
-    all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
-    all_label_ids = torch.tensor([f.label_id for f in features],
-                                 dtype=torch.float if multi_label else torch.long)
-    eval_data = {
-        "input_ids": all_input_ids,
-        "attention_mask": all_input_mask,
-        "token_type_ids": all_segment_ids
-    }
-
-    # Run prediction for full data
-    skorch_model = NeuralNetClassifier(model)
-    skorch_model = skorch_model.initialize()
-    all_probs = skorch_model.predict_proba(eval_data)
-
-    # # Write probabilities to file
-    # print("Writing probabilities to {}:".format(output_probs_file))
-    # all_probs_as_df = pd.DataFrame(data=all_probs, columns=label_list)
-    # all_probs_as_df.to_csv(output_probs_file, sep='\t')
-
-    return all_probs
 
 
 def pred_from_thresholds(prob, thresholds):
@@ -151,24 +112,20 @@ def pred_from_thresholds(prob, thresholds):
     return np.greater(prob, thresholds_mask).astype(int)
 
 
-# dev_prob = eval_and_predict(processor.get_dev_examples(DATA_DIR), True)
-# test_prob = eval_and_predict(processor.get_test_examples(DATA_DIR), True)
-
-
-demo_sentences = [
-    "可是 ， 这么 惬意 的 地方 ， 出现 了 这样 大煞风景 的 一幕 ? 唉 [ 害怕 ] [ 害怕 ] [ 害怕 ]",
-    "后面 才 是 亮点 … … @ BAAKOLAM   你 的 S3 借 我 耍耍 [ 泪 ]",
-    "因为 梦想 ， 所以 坚持   ! [ 心 ] / / @ 羊脂球 小姐 的 爱 : .... 这 妞儿   真 美 [ 花心 ]",
-]
-
-emotion_map = {i: label for i, label in enumerate(label_list)}
-
-demo_examples = [InputExample(i, sent) for i, sent in enumerate(demo_sentences)]
-demo_prob = eval_and_predict_skorch(demo_examples, True)
-# Assuming that class imbalance was compensated for in training stage, 0.5 is an acceptable global threshold
-demo_pred = pred_from_thresholds(demo_prob, [0.5]*len(label_list))
-demo_pred_labels = [[emotion_map[i] for i in np.nonzero(p)[0]] for p in demo_pred]
-print("Predicted emotions:")
-for i, sent in enumerate(demo_sentences):
-    print(f"{sent}\temotions: {demo_pred_labels[i]}")
+# demo_sentences = [
+#     "可是 ， 这么 惬意 的 地方 ， 出现 了 这样 大煞风景 的 一幕 ? 唉 [ 害怕 ] [ 害怕 ] [ 害怕 ]",
+#     "后面 才 是 亮点 … … @ BAAKOLAM   你 的 S3 借 我 耍耍 [ 泪 ]",
+#     "因为 梦想 ， 所以 坚持   ! [ 心 ] / / @ 羊脂球 小姐 的 爱 : .... 这 妞儿   真 美 [ 花心 ]",
+# ]
+#
+# emotion_map = {i: label for i, label in enumerate(label_list)}
+#
+# demo_examples = [InputExample(i, sent) for i, sent in enumerate(demo_sentences)]
+# demo_prob = eval_and_predict_skorch(demo_examples, True)
+# # Assuming that class imbalance was compensated for in training stage, 0.5 is an acceptable global threshold
+# demo_pred = pred_from_thresholds(demo_prob, [0.5]*len(label_list))
+# demo_pred_labels = [[emotion_map[i] for i in np.nonzero(p)[0]] for p in demo_pred]
+# print("Predicted emotions:")
+# for i, sent in enumerate(demo_sentences):
+#     print(f"{sent}\temotions: {demo_pred_labels[i]}")
 
